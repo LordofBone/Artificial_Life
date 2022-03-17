75f76ac8173840b116740a18388a7115723a212f
314Reactor <34923578+LordofBone@users.noreply.github.com>
2022-03-17 21:24:54 +0000

    Added in a cool shader that can do various things by changing certain variables

--- shaders.py
+++ shaders.py
@@ -13,6 +13,12 @@ class Shaders:
 
         self.max_coord_combine = max_coord_combine
 
+        self.count_number = 0
+        self.count_number_max = 256
+
+        self.addition_shader = True
+        self.invert_count = True
+
     # thanks to
     # https://stackoverflow.com/questions/52992900/how-to-blend-two-rgb-colors-front-and-back-based-on-their-alpha-channels
     def blend_shader(self, color_1, color_2, alpha):
@@ -41,10 +47,42 @@ class Shaders:
         shaded_colour = self.blend_shader(input_colour, blend_colour, alpha_custom)
         return shaded_colour
 
+    def number_count_step_plus(self):
+        if self.count_number < self.count_number_max:
+            self.count_number += 1
+        else:
+            if self.invert_count:
+                self.addition_shader = not self.addition_shader
+            else:
+                self.count_number = 0
+
+    def number_count_step_minus(self):
+        if self.count_number > 0:
+            self.count_number -= 1
+        else:
+            if self.invert_count:
+                self.addition_shader = not self.addition_shader
+            else:
+                self.count_number = self.count_number_max
+
     def full_screen_shader_gradient(self, coord, pixel):
         output_from_coord = coord[0] * coord[1]
 
         pixel_alpha = (output_from_coord / self.max_coord_combine) * self.max_colour
+        shaded_colour = self.custom_rgb_shader(pixel, self.base_green, pixel_alpha)
+
+        return shaded_colour
+
+    # found this accidentally; I am not entirely sure how this works, but changing the max number that can be stepped
+    # to will result in various cool effects as well as plus/minus stepping and whether the stepping will reset
+    # when maxed or count back the other way
+    def full_screen_shader_2(self, pixel):
+        if self.addition_shader:
+            self.number_count_step_plus()
+        else:
+            self.number_count_step_minus()
+
+        pixel_alpha = (self.count_number / self.count_number_max) * self.max_colour
         shaded_colour = self.custom_rgb_shader(pixel, self.base_green, pixel_alpha)
 
         return shaded_colour
